1d3f28a34fc818160da846f14fba0c9d433ff578
onaseer07 <onaseer07@gmail.com>
2018-05-14 23:52:43 -0500

    Finished the game!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,63 @@
 
 //Start
 //Computer selects a random alphabet;
+function startGame() {
 let letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 let computerChoices = letterChoices[Math.floor(Math.random()*letterChoices.length)];
+let lives = 9;
+let wins = 0;
+let losses = 0;
+let guessedList = [ ];
+
 alert("Computer picked " + " ' " + computerChoices + " ' ");
-//userguess could be any alphabet on the keyboard;
-document.onkeyup = function (event) {
-    let userGuess = event.key;
-    alert(userGuess);
-    let wins = 0;
-    let losses = 0;
-    let lives;
-    let guessedList = [];
-    
-    if (userGuess === computerChoices) {
-        document.getElementById("win").textContent = wins++;
-    } else (lives-1);
-    alert (lives);
+
+
+document.getElementById("losses").innerHTML = "Total Losses: " + losses;
+document.getElementById("lives").innerHTML = "Total Lives Left: " + lives;
+
+
+    do {
+        
+        document.onkeyup = function (event) {
+            let userGuess = event.key;
+        if (letterChoices.indexOf(userGuess) > -1) {
+            guessedList.push(userGuess);
+            document.getElementById("lettersGuessed").innerHTML = "Letter Guessed: [ " + guessedList + " ]";
+        
+            if (userGuess == computerChoices) {
+                wins++;  
+                document.getElementById("win").innerHTML = "Total Wins: " + wins;
+                let restartQuestion = confirm("PLAY AGAIN?");
+                if(restartQuestion == true) {
+                location.reload();
+                } else if (restartQuestion == false) {
+                alert("YOU MAY 'X' THIS WINDOW!");
+                }
+            } else if (userGuess !== computerChoices) {
+                lives--;
+                alert("Total Lives: " + lives);
+                if (lives == 0) {
+                    alert("GAMEOVER");
+                    let restartQuestion = confirm("PLAY AGAIN?");
+                    if(restartQuestion == true) {
+                    location.reload();
+                    } else if (restartQuestion == false) {
+                    alert("YOU MAY 'X' THIS WINDOW!");
+                    }
+                }
+            }
+        }
+    }; 
+    }while (lives == 0);
 }
 
-//selected alphabet gets stored as a value to check against userguess;
 
-//if the userguessed letter matches computer's selected alphabet then user wins else the lives go down by 1 out 8.;
 
-//when live is less than 8 the game is over and the user is lost ;
+/*/if the userguessed letter matches computer's selected alphabet then user wins else the lives go down by 1 out 8.;
 
-//display guesssed letters;
+//when live is less than 9 the game is over and the user is lost;
+
+//display guessed letters;
 //display lives left counter;
 //display # of wins:;
-//display # of losse:;
+//display # of losses:*/
